f9caf012e9a6ca06cda6aa7bd9120b14229da6cd
nidhish654 <nidhish.shettigar.06@gmail.com>
2024-10-23 01:45:09 +0530

    new changes

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <main>
-  <div>
+  <div class = "bg-[#be8282]">
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
@@ -13,7 +13,7 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1 class= "text-red-700">Vite + Svelte</h1>
+  <h1 class= "text-blue-500">Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
@@ -23,15 +23,15 @@
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
+  <p class="text-[#888888]">
     Click on the Vite and Svelte logos to learn more
   </p>
 </main>
 
 <style>
   .logo {
-    height: 6em;
-    padding: 1.5em;
+    height: 10em;
+    padding: 3em;
     will-change: filter;
     transition: filter 300ms;
   }
@@ -40,8 +40,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
